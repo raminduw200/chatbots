580af140a977d2bc4fc507d8b4ba0bff8578f900
raminduw200 <raminduw200@gmail.com>
2021-06-09 11:26:17 +0530

    Add keyboard interupt

--- spam_bot/spam_bot.py
+++ spam_bot/spam_bot.py
@@ -1,8 +1,12 @@
 import pyautogui
 import time
+import keyboard
 
 time.sleep(5);
-f = open("spam_script.txt", 'r')
+f = open("spam_bot/spam_script.txt", 'r')
+
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press('enter')
+    if keyboard.is_pressed('q'):
+        break
